a39bd59fbe2b8bca42d8140116900a890a2e38e2
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-08-21 19:39:29 -0300

    atividade02 finalizada

--- atividade02.py
+++ atividade02.py
@@ -11,5 +11,29 @@ def conversor_moedas(valor, moeda_origem, moeda_destino):
         return round(valor, 2)
     return "Moeda não suportada"
 
-print(conversor_moedas(100, "real", "real"))
+print(conversor_moedas(100, "real", "dolar"))
 
+def calculadora_desconto(valor, percentual):
+    percentual = percentual / 100
+    preco_final = valor - (valor * percentual)
+    return f" o preço final de seu item é R${round(preco_final, 2)}. Você teve um desconto de R${round(valor * percentual, 2)}"
+
+print(calculadora_desconto(50, 20))
+
+def calculadora_media_escolar(notas):
+    if notas == []:
+        return "Nenhuma nota disponível para cálculo."
+    media = sum(notas) / len(notas)
+    for nota in notas:
+        print(f"Nota: {nota}")
+    return f"A média das notas é {round(media, 2)}"
+
+print(calculadora_media_escolar([7.5, 8.0, 6.5]))
+
+def calculadora_consumo_combustivel(distancia_percorrida, combustivel_gasto):
+    if combustivel_gasto == 0:
+        return "O consumo de combustível não pode ser zero."
+    consumo = distancia_percorrida / combustivel_gasto
+    return f"Você percorreu {distancia_percorrida}km, gastando um total de {combustivel_gasto} litros de combustível. O consumo médio de combustível é {round(consumo, 2)} km/l."
+
+print(calculadora_consumo_combustivel(300, 25))
